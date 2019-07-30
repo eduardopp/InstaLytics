ec91626398709a84f911a90d5d7437c5802d0895
eduardopp <eduardoppereira98@gmail.com>
2019-07-30 22:50:16 +0100

    Phase 1 -. Analytics

---
+++ start.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class InstagramBot:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome(executable_path='/home/eduardo/Desktop/chromedriver')
+
+
+    def closeBrowser(self):
+        self.driver.close()
+
+    def login(self):
+
+        self.driver.get('https://www.instagram.com/')
+        time.sleep(2)
+
+        loginBtn = self.driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
+        loginBtn.click()
+        time.sleep(2)
+
+        inputUser = self.driver.find_element_by_xpath("//input[@name='username']")
+        inputUser.clear()
+        inputUser.send_keys(self.username)
+
+        inputPass = self.driver.find_element_by_xpath("//input[@name='password']")
+        inputPass.clear()
+        inputPass.send_keys(self.password)
+        inputPass.send_keys(Keys.RETURN)
+        time.sleep(2)
+
+    def myProfile(self):
+
+        self.driver.find_element_by_css_selector('.aOOlW.HoLwm').click()
+        
+        profileBtn = self.driver.find_element_by_xpath("//a[@href='/eduardpereira/']")
+        profileBtn.click()
+
+        print(len("LEN:" + str(self.driver.find_element_by_css_selector('.9AhH0'))))
+
+
+        
+
+
+
+
+bot = InstagramBot('eduardpereira', 'b0dc08c9e5ba')
+bot.login()
+bot.myProfile()
